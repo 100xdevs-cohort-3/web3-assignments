32fe214f24590b00628e990415245bed3dbdfd65
Tridib11 <tridibghosh12345@gmail.com>
2024-11-04 06:01:17 +0530

    Week1 Assignments solved

--- week-01/assignment-1.js
+++ week-01/assignment-1.js
@@ -15,16 +15,19 @@ Output: 00000691457f4f0ce13e187b9ab4fda6d42c8647752909b8f71f9dbd8f6bd4ab
 */
 
 // Function to find an input string that produces a hash starting with '00000'
-function findHashWithPrefix(prefix) {
-    // initilize input with the correct value.
-    
-    let input; // change here
-    while (true) {
-       // logic here 
+const crypto=require("crypto")
+function findHashWithPrefix(prefix){
+  let input=0;
+  while(true){
+    let inputStr=input.toString();
+    let hash=crypto.createHash('sha256').update(inputStr).digest('hex')
+    if(hash.startsWith(prefix)){
+      return {input:inputStr,hash:hash}
     }
+    input++;
+  }
 }
 
-// Find and print the input string and hash
-const result = findHashWithPrefix('00000');
-console.log(`Input: ${result.input}`);
-console.log(`Output Hash: ${result.hash}`);
+const result=findHashWithPrefix("00000")
+console.log(`Input ${result.input}`)
+console.log(`Hash ${result.hash}`)
--- week-01/assignment-2.js
+++ week-01/assignment-2.js
@@ -12,20 +12,21 @@ Output Hash: 00000b2d1dde70b56f41571855e268d140b304c7661c0ef04a8889bd83a88b06
 */
 
 // Function to find an input string that produces a hash starting with '00000'
-function findHashWithPrefix(prefix) {
-    let input = 0;
-    while (true) {
-        // can you guess what we need to add ??
-        let inputStr;   // you need to change here
-        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
-        if (hash.startsWith(prefix)) {
-            return { input: inputStr, hash: hash };
-        }
-        input++;
+const crypto=require("crypto")
+function findingHashWithprefix(prefix){
+  let input=0
+  while(true){
+    let hash="100xdevs"+input.toString()
+    let hashedValue=crypto.createHash('sha256').update(hash).digest('hex')
+    if(hashedValue.startsWith(prefix)){
+      return {input:hash, hashedValue:hashedValue}
     }
+    input++;
+  }
+  
 }
 
-// Find and print the input string and hash
-const result = findHashWithPrefix('00000');
-console.log(`Input: ${result.input}`);
-console.log(`Hash: ${result.hash}`);
+
+const result=findingHashWithprefix("00000")
+console.log(`Input ${result.input}`)
+console.log(`Hash ${result.hashedValue}`)
